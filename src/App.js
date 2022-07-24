0da9abdb35798829873388f63bcfda55c8717867
Vegard Lillesalt Valvik <vegard.valvik@gmail.com>
2022-07-24 20:50:26 +0200

    Create file structure, header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Header from "./Header"
+import Card from "./Card"
+import Style from "./style.css"
+
+export default function () {
+	return (
+			<div>
+				<Header />
+				<Card />
+			</div>
+	)
+}
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function () {
+	return (
+		<nav>
+			<i class="fa-solid fa-earth-asia fa-xl"></i><h3 className="nav--text">my travel journal.</h3>
+
+		</nav>
+	)
+}
