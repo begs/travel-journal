34f7cda699b2b8cef1501800d7c50910212b4664
Vegard Lillesalt Valvik <vegard.valvik@gmail.com>
2022-07-27 11:46:26 +0200

    Finish card layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import Header from "./Header"
 import Card from "./Card"
 import Style from "./style.css"
+import Data from "./data"
 
 export default function App() {
 	return (
--- src/Card.js
+++ src/Card.js
@@ -7,9 +7,9 @@ export default function () {
 				<h3>pic</h3>
 			</div>
 			<div className="text-container">
-				<p className="country-text">JAPAN</p>
+				<p><span className="country-text"><i class="fa-solid fa-location-dot"></i>JAPAN</span><span className="maps-link">View on Google Maps</span></p>
 				<p className="destination-text">Mount Fuji</p>
-				<p className="date-text">Dato</p>
+				<p className="date-text">12 Jan, 2021 - 24 Jan, 2021</p>
 				<p className="description-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
 			</div>
 		</div>
