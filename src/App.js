16df98a48fcd9e93aaa5b610055a0b7b839ff350
Vegard Lillesalt Valvik <vegard.valvik@gmail.com>
2022-07-29 14:02:27 +0200

    Fix image scrolling, add gmaps link, add breakline logic

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ import Header from "./Header"
 import Card from "./Card"
 import Style from "./style.css"
 import Data from "./Data"
+import Breakline from "./Breakline"
 
 export default function App() {
-	const cards = Data.map(item => {
-		return (
-			<Card 
-				key={item.id}
-				{...item}
-			/>
-		)
-	})
+	const cards = Data.map((item, i, arr) => {
+		if (arr.length -1 === i){
+			// last item in array, return with breakline
+			return (
+				<Card 
+					key={item.id}
+					{...item}
+				/>
+			)
+		} else {
+			// not last item, return without breakline
+			return (
+				<div>
+					<Card
+						key={item.id}
+						{...item}
+					/>
+					<Breakline />
+				</div>
+			)
+		}
+	});
+
 	return (
 			<div>
 				<Header />
--- src/Card.js
+++ src/Card.js
@@ -7,7 +7,7 @@ export default function (props) {
 				<img className="location-pic" src={props.imageUrl}></img>
 			</div>
 			<div className="text-container">
-				<p><span className="country-text"><i class="fa-solid fa-location-dot"></i>{props.location}</span><span className="maps-link">View on Google Maps</span></p>
+				<p><span className="country-text"><i class="fa-solid fa-location-dot"></i>{props.location}</span><a href={props.googleMapsUrl} target="_blank" className="maps-link">View on Google Maps</a></p>
 				<p className="destination-text">{props.title}</p>
 				<p className="date-text">{props.startDate} - {props.endDate}</p>
 				<p className="description-text">{props.description}</p>
