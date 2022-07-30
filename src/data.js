51f57c42f289e5ae66668dd1e78a11917da32b65
Vegard Lillesalt Valvik <vegard.valvik@gmail.com>
2022-07-30 18:12:50 +0200

    Add new data, small layout adjustments

--- src/data.js
+++ src/data.js
@@ -1,22 +1,32 @@
 export default [
     {
         id: 1,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "SignaTUR i Tafjordfjella",
+        location: "Møre og Romsdal, Norway",
+        googleMapsUrl: "https://goo.gl/maps/dxtmPxERuKLuWVcs5",
+        startDate: "02 juli, 2022",
+        endDate: "09 juli, 2022",
+        description: "SignaTUR i Tafjordfjella er ei rute satt opp av DNT som går gjennom fjella midt i hjertet av Reinheimen. Turen går gjennom variert terreng og er totalt på over 70 km i avstand og med over 3600 høydemeter.",
+        imageUrl: "https://i.imgur.com/3c6mUSY.png"
     },
     {
         id: 2,
-        title: "Mount Luigi",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Utrecht",
+        location: "Nederland",
+        googleMapsUrl: "https://goo.gl/maps/7Wf3WhMUn2NAWKvm8",
+        startDate: "11 april, 2022",
+        endDate: "14 april, 2022",
+        description: "Utrecht er en koselig by midt i Nederland. Her kan man nyte rolige dager på kafèer langs kanalen, eller sykkelturer i den bilfrie sentrumskjerna.",
+        imageUrl: "https://i.imgur.com/3GTH7dQ.png"
+    },
+    {
+        id: 3,
+        title: "Vetvika",
+        location: "Bremanger, Norge",
+        googleMapsUrl: "https://goo.gl/maps/beN6TadYaKQFSLmV8",
+        startDate: "28 august, 2021",
+        endDate: "29 august, 2021",
+        description: "Vetvika er ei forblåst og veglaus strand i Bremanger i Sogn og Fjordane. Einaste veg til stranda er enten med båt eller over fjellet. Vetvika blir ofte kalla ei 'skjult naturperle', og er ein flott rundtur i kjempefint vestlandsterreng ⛰",
+        imageUrl: "https://i.imgur.com/O4k9txy.png"
     }
 ]
