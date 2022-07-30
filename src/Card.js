12395e1b60f632bb5605752e382a4b83aa4a1a6a
Vegard Lillesalt Valvik <vegard.valvik@gmail.com>
2022-07-30 18:21:00 +0200

    Add alt text, html doctype

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ export default function (props) {
 	return (
 		<div className="card">
 			<div className="pic-container">
-				<img className="location-pic" src={props.imageUrl}></img>
+				<img className="location-pic" src={props.imageUrl} alt={props.imageAltText}></img>
 			</div>
 			<div className="text-container">
 				<p><span className="country-text"><i class="fa-solid fa-location-dot"></i>{props.location}</span><a href={props.googleMapsUrl} target="_blank" className="maps-link">View on Google Maps</a></p>
--- src/data.js
+++ src/data.js
@@ -7,7 +7,8 @@ export default [
         startDate: "02 juli, 2022",
         endDate: "09 juli, 2022",
         description: "SignaTUR i Tafjordfjella er ei rute satt opp av DNT som går gjennom fjella midt i hjertet av Reinheimen. Turen går gjennom variert terreng og er totalt på over 70 km i avstand og med over 3600 høydemeter.",
-        imageUrl: "https://i.imgur.com/3c6mUSY.png"
+        imageUrl: "https://i.imgur.com/3c6mUSY.png",
+        imageAltText: "En varde og fjellutsikt"
     },
     {
         id: 2,
@@ -17,7 +18,8 @@ export default [
         startDate: "11 april, 2022",
         endDate: "14 april, 2022",
         description: "Utrecht er en koselig by midt i Nederland. Her kan man nyte rolige dager på kafèer langs kanalen, eller sykkelturer i den bilfrie sentrumskjerna.",
-        imageUrl: "https://i.imgur.com/3GTH7dQ.png"
+        imageUrl: "https://i.imgur.com/3GTH7dQ.png",
+        imageAltText: "Kanal rundt bykjerna i Utrecht"
     },
     {
         id: 3,
@@ -27,6 +29,7 @@ export default [
         startDate: "28 august, 2021",
         endDate: "29 august, 2021",
         description: "Vetvika er ei forblåst og veglaus strand i Bremanger i Sogn og Fjordane. Einaste veg til stranda er enten med båt eller over fjellet. Vetvika blir ofte kalla ei 'skjult naturperle', og er ein flott rundtur i kjempefint vestlandsterreng ⛰",
-        imageUrl: "https://i.imgur.com/O4k9txy.png"
+        imageUrl: "https://i.imgur.com/O4k9txy.png",
+        imageAltText: "Blå himmel og utsikt over stranda i Vetvika"
     }
 ]
